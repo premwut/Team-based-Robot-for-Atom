de61125ac0915f8f9fcc6e4a39fa8ab3f9d5fcdf
Premwut Klaychim <59070104@it.kmitl.ac.th>
2019-10-31 22:28:16 +0700

    Edit Testcase API routing

--- team-based-robot-server/server/routes/testcase.router.js
+++ team-based-robot-server/server/routes/testcase.router.js
@@ -2,11 +2,12 @@ import { FeatureType, PermissionType, TESTCASE_API } from "../utilities/constant
 import { Router } from "express"
 import { verifyAuthentication as authn } from "../utilities/authentication"
 import { verifyAuthorization as authz } from "../utilities/authorization"
+import Testcase from "../controllers/testcase.controller"
 
 const router = Router()
+const ctrl = new Testcase()
 
-router.get(`${TESTCASE_API}/list`, (req, res) => {
-  res.send("<h1>Hello World!</h1>")
-})
+router.get(`${TESTCASE_API}/list`, ctrl.get.bind(ctrl))
+router.post(`${TESTCASE_API}/create`, ctrl.create.bind(ctrl))
 
 export default router
