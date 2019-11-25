a20f5293e05bd1b0a9ed153289f0737efac2396d
Panudet Sukontasuchot <59070130@it.kmitl.ac.th>
2019-11-25 12:08:21 +0700

    test store module

--- team-based-robot-server/store/keyword.js
+++ team-based-robot-server/store/keyword.js
@@ -3,6 +3,7 @@ export const state = () => ({
 })
 
 export const mutations = {
+  // set keywords into store
   setKeywords (state, data) {
     state.keywords = {
       ...data,
@@ -10,6 +11,7 @@ export const mutations = {
     }
     delete state.keywords.keywords
   },
+  // set the  owner of keywords
   applyKeywords (state, { keywords = [] } = {}) {
     const keywordList = state.keywords.list
     if (keywordList && keywordList.length > 0) {
@@ -20,7 +22,7 @@ export const mutations = {
     }
   },
 }
-
+// get keywords from database then [setKeywords] and [verifyKeywords]
 export const actions = {
   async fetchKeywords ({ commit, dispatch }, { page = 1, limit = 10 } = {}) {
     try {
@@ -32,6 +34,7 @@ export const actions = {
       throw error
     }
   },
+  // get keyword name that verify by user id from database then [applyKeywords]
   async verifyKeywords ({ commit }, keywordNames = []) {
     try {
       const { data } = await this.$axios.$post("/api/keyword/name/verify", { keywordNames })
@@ -40,6 +43,7 @@ export const actions = {
       throw error
     }
   },
+  // delete keyword in database then [fetchKeywords]
   async deleteKeyword ({ dispatch }, params) {
     try {
       const { kwd_id } = params
@@ -51,7 +55,7 @@ export const actions = {
     }
   },
 }
-
+// get keywords from store
 export const getters = {
   getKeywords (state) {
     return state.keywords
