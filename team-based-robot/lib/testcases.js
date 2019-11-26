2e909d476a95c7d3fcc1ebde75f6f6af1610c785
Premwut Klaychim <59070104@it.kmitl.ac.th>
2019-11-27 00:10:28 +0700

    Edit saveTestcase function.
    to support editing (put method).

--- team-based-robot/lib/testcases.js
+++ team-based-robot/lib/testcases.js
@@ -26,38 +26,69 @@ export const saveTestcase = async (tbInstance) => {
   let testResults = getTestResults().robot
   const connection = tbInstance.connection
   const currentUser = await connection.getProfile()
-  console.log(currentUser)
   testResults = (testResults.suite.suite) ? testResults.suite.suite : testResults.suite
+
+  let nonDuplicateTc
+  const test = await connection.getTestcase()
+  const testcaseData = test.data.testcases //fetched from database
 
   if (!Array.isArray(testResults)) {
     testResults = [ testResults ]
   }
 
-  let outputData = { testcases: [], usr_id: currentUser.usr_id }
+  let resultTestcase = []
+      saveList = []
+      editList = []
   testResults.map(suite => {
-    console.log(suite, 'suite')
-    let testcases = (Array.isArray(suite.test)) ? suite.test : [ suite.test ]
-    testcases.forEach(testcase => {
-      let tcStatus = testcase.status._attributes
-      let tcRunDate = tcStatus.starttime.split('')
-      tcRunDate = [ [tcRunDate[0], tcRunDate[1], tcRunDate[2], tcRunDate[3]].join(''),
-      [ tcRunDate[4], tcRunDate[5] ].join(''), [tcRunDate[6], tcRunDate[7]].join('') ].join('/')
-
-      let input = {
-          name: testcase._attributes.name,
-          result: (tcStatus.status) === 'PASS' ? true : false,
-          start: tcStatus.starttime.split(' ')[1],
-          end: tcStatus.endtime.split(' ')[1],
-          date: tcRunDate,
+    let { test } = suite
+    if (!Array.isArray(test)) test = [ test ]
+    test.map(testcase => resultTestcase.push(testcase))
+  })
+  console.log(resultTestcase)
+  resultTestcase.map(result => {
+    let resultName = result._attributes.name
+    let isDuplicated = false
+    testcaseData.map(data => {
+      let dataName = data.tc_name
+      if (dataName === resultName) {
+        isDuplicated = true
       }
-      outputData.testcases.push(input)
+      console.log(`${dataName} === ${resultName}`, dataName === resultName)
     })
 
+    if (isDuplicated) {
+      editList.push(result)
+    } else {
+      saveList.push(result)
+    }
   })
 
+  console.log(testResults, 'testResults')
+  console.log(testcaseData, 'testcaseData')
+  console.log(saveList, 'saveList')
+  console.log(editList, 'editList')
+
+  let outputData = { testcases: [], usr_id: currentUser.usr_id }
+  saveList = saveList.map(testcase => {
+    let tcStatus = testcase.status._attributes
+    let tcRunDate = tcStatus.starttime.split('')
+    tcRunDate = [ [tcRunDate[0], tcRunDate[1], tcRunDate[2], tcRunDate[3]].join(''),
+    [ tcRunDate[4], tcRunDate[5] ].join(''), [tcRunDate[6], tcRunDate[7]].join('') ].join('/')
+
+        let input = {
+            name: testcase._attributes.name,
+            result: (tcStatus.status) === 'PASS' ? true : false,
+            start: tcStatus.starttime.split(' ')[1],
+            end: tcStatus.endtime.split(' ')[1],
+            date: tcRunDate,
+        }
+        outputData.testcases.push(input)
+  })
+  
   console.log(outputData)
+
+
   const { data: response } = await connection.saveTestcaseRun(outputData)
-  console.log(response, 'response')
 
   return outputData
 }
