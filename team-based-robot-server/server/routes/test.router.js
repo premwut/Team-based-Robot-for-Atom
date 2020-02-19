c3d49f015e7a0aca9cd314367501ca01cd4886f0
panudet40 <59070130@it.kmitl.ac.th>
2020-02-20 04:12:18 +0700

    fix constants

--- team-based-robot-server/server/routes/test.router.js
+++ team-based-robot-server/server/routes/test.router.js
@@ -1,2 +1,19 @@
 import { FeatureType, TEST_API, PermissionType } from "../utilities/constants"
 
+import TestController from "../controllers/test.controller"
+import { Router } from "express"
+import { verifyAuthentication as authn } from "../utilities/authentication"
+import { verifyAuthorization as authz } from "../utilities/authorization"
+
+const router = Router()
+const ctrl = new TestController()
+const manageWrite = [
+    [FeatureType.ALL, PermissionType.WRITE],
+    [FeatureType.MANAGE_TEST, PermissionType.WRITE],
+]
+const manageRead = [
+    ...manageWrite,
+    [FeatureType.MANAGE_TEST, PermissionType.READ],
+]
+  
+console.log("Here!, I'm in test router")
