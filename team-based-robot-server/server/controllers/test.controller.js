6d8c7a7d93baabaebb737c9d25146d3af6d854ef
panudet40 <59070130@it.kmitl.ac.th>
2020-02-25 20:40:02 +0700

    Update test.controller.js

--- team-based-robot-server/server/controllers/test.controller.js
+++ team-based-robot-server/server/controllers/test.controller.js
@@ -11,9 +11,24 @@ import bookshelf from "../config/bookshelf"
 export default class TestController extends BaseController {
     async create (req, res) {
         try {
-            const
+            const { test_tc_no, test_passed, test_failed, test_file_link, test_result } = req.body
+            const { usr_id } = req.currentUser
+            const testData = {
+                usr_id,
+                test_tc_no,
+                test_failed,
+                test_file_link
+            }
+            const testMapCollection = TestMappings.forge(testData).save()
+            .then(result => {
+                const testMapCollection = TestMappings.forge()
+                test_result.forEach(kwd, index => {
+                    const { tc_name, tc_passed, kwd_list } = element
+                    const test = await this.convertToTest(index, element)  
+                });
+            })
         } catch (error) {
-            
+            console.log(error)
         }
     }
 
@@ -31,5 +46,15 @@ export default class TestController extends BaseController {
 
     async saveTestMapping(tx, testCollection) {
 
+    }
+
+    convertToTest (index, data) {
+        if (!data) return undefined
+        const {
+            tc_name,
+            tc_passed,
+            kwd_list
+        } = data
+        return Test
     }
 }
