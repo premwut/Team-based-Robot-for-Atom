1c4bc869ddc40dc9d7b45d76995fd8f384d8ba50
panudet40 <59070130@it.kmitl.ac.th>
2020-03-01 21:04:33 +0700

    test clean code

--- team-based-robot-server/server/controllers/test.controller.js
+++ team-based-robot-server/server/controllers/test.controller.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import Test, { Tests } from "../models/test.model"
 import TestMapping, { TestMappings } from "../models/testMapping.model"
 import Keyword, { Keywords } from "../models/keyword.model"
@@ -9,135 +10,108 @@ import { Users } from "../models/user.model"
 import bookshelf from "../config/bookshelf"
 
 export default class TestController extends BaseController {
-    async create (req, res) {
-        try {
-            const { test_tc_no, test_passed, test_failed, test_file_link, test_result } = req.body
-            const { usr_id } = req.currentUser
-            const testMapCollection = TestMappings.forge()
-            console.log("usr_id =", usr_id)
-            const testData = {
-                test_tc_no,
-                test_passed,
-                test_failed,
-                test_file_link,
-                usr_id,
-            }
-            const test = await Test.forge(testData).save()
-            const testId = test.get(Fields.TEST_ID)
+  async create (req, res) {
+    try {
+      const { test_tc_no, test_passed, test_failed, test_file_link, test_result } = req.body
+      const { usr_id } = req.currentUser
+      console.log("usr_id =", usr_id)
+      const testData = {
+        test_tc_no,
+        test_passed,
+        test_failed,
+        test_file_link,
+        usr_id,
+      }
+      const test = await Test.forge(testData).save()
+      const testId = test.get(Fields.TEST_ID)
 
-            // await Promise.all(test_result.map( async (testcase, tcId) => { 
-            //     testcase.tc_id = tcId + 1
-            //     const { tc_id, kwd_list } = testcase
-            //     const keywordNames = R.uniq(kwd_list)
-            //     const queryKeyword = q => q.where(Fields.KWD_NAME, "in", R.uniq(keywordNames))
+      // Lab by P'Golf Yossapol
+      const promiseKeywords = test_result
+        .map(({kwd_list}) => R.uniq(kwd_list))
+        .map(keywordName => q => q.where(Fields.KWD_NAME, "in", R.uniq(keywordName)))
+        .map((queryKeyword) => Keywords.query(queryKeyword).fetch({require: false}))
 
-            //     const keywords =  await Keywords.query(queryKeyword).fetch() //[require=true]
+      const fetchKeywords = await Promise.all(promiseKeywords)
 
-            //     keywords.map((kwd) => console.log("kwd.length =", kwd.get(Fields.KWD_NAME)))
+      const fetchKeywordMapping = fetchKeywords.reduce((acc, fetchKeyword) => {
+        fetchKeyword.forEach(kwd => {
+          const kwdName = kwd.get(Fields.KWD_NAME)
 
-            //     keywords.map((kwd) => {
-            //         const testMapping = this.convertToTestMapping(kwd, testId, tc_id, testcase)
-            //         this.logTestMapping(testMapping)
-            //         testMapCollection.push(testMapping)
-            //     })
-            // }))
-            // console.log('list ==> ', list)
-            // await Promise.all(list)
-
-            // Lab by P'Golf Yossapol
-            const promiseKeywords = test_result
-                .map(({kwd_list}) =>  R.uniq(kwd_list))
-                .map(keywordName => q => q.where(Fields.KWD_NAME, "in", R.uniq(keywordName)))
-                .map((queryKeyword) => Keywords.query(queryKeyword).fetch({require: false}))
-
-            const fetchKeywords = await Promise.all(promiseKeywords)
-
-            const fetchKeywordMapping = fetchKeywords.reduce((acc, fetchKeyword) => {
-                fetchKeyword.forEach(kwd => {
-                    const kwdName = kwd.get(Fields.KWD_NAME)
-
-                    if (!R.isNil(kwdName)) {
-                        acc[kwdName] = kwd
-                    }
-                })
-                return acc
-            }, {})
-
-            const testMapCollection3 = test_result.reduce((acc, testcase, tcId) => {
-                testcase.tc_id = tcId + 1
-                const { tc_id, kwd_list } = testcase
-                const keywordNames = R.uniq(kwd_list)
-
-                const testMap = R.flatten(keywordNames.map((kwdName) => {
-                    const kwd = fetchKeywordMapping[kwdName]
-                    return this.convertToTestMapping(kwd, testId, tc_id, testcase, kwdName)
-                }))
-
-                return acc.push(testMap)
-            }, TestMappings.forge())
-
-
-
-            const data = await bookshelf.transaction( async (tx) => {
-                // console.log("testMapColl =", testMapCollection)
-                testMapCollection3.forEach(testMap => this.logTestMapping(testMap))
-                const results = await testMapCollection3.invokeThen("save", null, {transacting: tx})
-                console.log('results ===> ', results.length)
-                return results 
-            })
-
-          this.success(res, data)
-        } catch (error) {
-          this.failure(res, error)
-        }
-    }
-
-    // async getList (req, res) {
-    //     try {
-    //         const { usr_id, team_id } = req.currentUser.toJSON()
-    //         const { date } = req.query
-    //         const date = new Date(date)
-    //         console.log('Date =', date)
-    //         const tests = await Test.forge()
-    //     } catch (error) {
-    //         this.failure(res, error)
-    //     }
-    // }
-
-    // async saveTestMapping(tx, testCollection) {
-
-    // }
-
-    convertToTestMapping (kwd, testId, tcId, testcase, name) {
-        // console.log(`In convertTestMap tcId = ${tcId}`)
-        let kwdId = null
-        let kwdName = null
-        const { tc_name, tc_passed } = testcase
-
-        if(R.isNil(kwd)) {
-            kwdName = name
-        }
-        else if (kwd.get(Fields.KWD_NAME) != null) {
-            kwdName = kwd.get(Fields.KWD_NAME)
-            kwdId = kwd.get(Fields.KWD_ID)
-        }
-        return TestMapping.forge({ 
-             kwd_id: kwdId,
-             test_id: testId, 
-             test_map_tc_id: tcId, 
-             test_map_tc_name: tc_name,
-             test_map_tc_passed: tc_passed,
-             test_kwd_name: kwdName,
+          if (!R.isNil(kwdName)) {
+            acc[kwdName] = kwd
+          }
         })
-    }
+        return acc
+      }, {})
 
-    logTestMapping(testMapping) {
-        let tcId = testMapping.get(Fields.TEST_MAP_TC_ID)
-        let tcName = testMapping.get(Fields.TEST_MAP_TC_NAME)
-        let tcPassed = testMapping.get(Fields.TEST_MAP_TC_PASSED)
-        let tcKwdId = testMapping.get(Fields.KWD_ID)
-        let tcKwdName = testMapping.get(Fields.TEST_KWD_NAME)
+      const testMapCollection3 = test_result.reduce((acc, testcase, tcId) => {
+        testcase.tc_id = tcId + 1
+        const { tc_id, kwd_list } = testcase
+        const keywordNames = R.uniq(kwd_list)
 
-        console.log(`testMapping => id:${tcId}, name:${tcName}, passed:${tcPassed}, kwdId:${tcKwdId}, kwdName:${tcKwdName}`)
+        const testMap = R.flatten(keywordNames.map((kwdName) => {
+          const kwd = fetchKeywordMapping[kwdName]
+          return this.convertToTestMapping(kwd, testId, tc_id, testcase, kwdName)
+        }))
+
+        return acc.push(testMap)
+      }, TestMappings.forge())
+
+      const data = await bookshelf.transaction(async (tx) => {
+        // console.log("testMapColl =", testMapCollection)
+        testMapCollection3.forEach(testMap => this.logTestMapping(testMap))
+        const results = await testMapCollection3.invokeThen("save", null, {transacting: tx})
+        console.log("results ===> ", results.length)
+        return results
+      })
+
+      this.success(res, data)
+    } catch (error) {
+      this.failure(res, error)
     }
+  }
+
+  // async getList (req, res) {
+  //     try {
+  //         const { usr_id, team_id } = req.currentUser.toJSON()
+  //         const { date } = req.query
+  //         const date = new Date(date)
+  //         console.log('Date =', date)
+  //         const tests = await Test.forge()
+  //     } catch (error) {
+  //         this.failure(res, error)
+  //     }
+  // }
+
+  convertToTestMapping (kwd, testId, tcId, testcase, name) {
+    // console.log(`In convertTestMap tcId = ${tcId}`)
+    let kwdId = null
+    let kwdName = null
+    const { tc_name, tc_passed } = testcase
+
+    if (R.isNil(kwd)) {
+      kwdName = name
+    } else if (kwd.get(Fields.KWD_NAME) != null) {
+      kwdName = kwd.get(Fields.KWD_NAME)
+      kwdId = kwd.get(Fields.KWD_ID)
+    }
+    return TestMapping.forge({
+      kwd_id: kwdId,
+      test_id: testId,
+      test_map_tc_id: tcId,
+      test_map_tc_name: tc_name,
+      test_map_tc_passed: tc_passed,
+      test_kwd_name: kwdName,
+    })
+  }
+
+  logTestMapping (testMapping) {
+    let tcId = testMapping.get(Fields.TEST_MAP_TC_ID)
+    let tcName = testMapping.get(Fields.TEST_MAP_TC_NAME)
+    let tcPassed = testMapping.get(Fields.TEST_MAP_TC_PASSED)
+    let tcKwdId = testMapping.get(Fields.KWD_ID)
+    let tcKwdName = testMapping.get(Fields.TEST_KWD_NAME)
+
+    console.log(`testMapping => id:${tcId}, name:${tcName}, passed:${tcPassed}, kwdId:${tcKwdId}, kwdName:${tcKwdName}`)
+  }
 }
--- team-based-robot-server/server/migrations/20200219221515_testMapping.js
+++ team-based-robot-server/server/migrations/20200219221515_testMapping.js
@@ -1,22 +1,22 @@
 const tableName = "test_mapping"
 
-exports.up = function(knex, Promise) {
-    return Promise.all([
-        knex.schema.createTable(tableName, (table) => {
-          table.increments("test_map_id")
-          table.string("test_kwd_name")
-          table.integer("test_map_tc_id")
-          table.string("test_map_tc_name")
-          table.boolean("test_map_tc_passed")
-          table.integer("test_id").references("tests.test_id")
-          table.integer("kwd_id").references("keywords.kwd_id")
-          table.timestamps()
-        }),
-      ])
+exports.up = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable(tableName, (table) => {
+      table.increments("test_map_id")
+      table.string("test_kwd_name")
+      table.integer("test_map_tc_id")
+      table.string("test_map_tc_name")
+      table.boolean("test_map_tc_passed")
+      table.integer("test_id").references("tests.test_id")
+      table.integer("kwd_id").references("keywords.kwd_id")
+      table.timestamps()
+    }),
+  ])
 }
 
-exports.down = function(knex, Promise) {
-    return Promise.all([
-        knex.schema.dropTable(tableName),
-      ])
+exports.down = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable(tableName),
+  ])
 }
