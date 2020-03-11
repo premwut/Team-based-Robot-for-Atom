9a47aa56e6203c00f271500f5867219888a72dc2
Premwut Klaychim <59070104@it.kmitl.ac.th>
2020-03-12 01:44:44 +0700

    Review authorization update

--- team-based-robot-server/server/routes/review.router.js
+++ team-based-robot-server/server/routes/review.router.js
@@ -8,10 +8,17 @@ import { verifyAuthorization as authz } from "../utilities/authorization"
 const router = Router()
 const ctrl = new ReviewController()
 
-const manageWrite = []
+const manageWrite = [
+  [FeatureType.ALL, PermissionType.WRITE],
+  [FeatureType.MANAGE_REVIEW, PermissionType.WRITE],
+]
+const manageRead = [
+  ...manageWrite,
+  [FeatureType.MANAGE_REVIEW, PermissionType.READ],
+]
 
 // router.get(`${REVIEW_API}/get`, ctrl.getReview.bind(ctrl))
-router.get(`${REVIEW_API}/get`, ctrl.getReview.bind(ctrl))
+router.get(`${REVIEW_API}/get`, authn, authz(manageRead), ctrl.getReview.bind(ctrl))
 router.post(`${REVIEW_API}/submit`, ctrl.submitReview.bind(ctrl))
 
 export default router
