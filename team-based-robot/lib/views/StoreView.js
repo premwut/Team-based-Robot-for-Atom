6d3707d4add2a886c896fb6d3da6a904715aaa7d
Premwut Klaychim <59070104@it.kmitl.ac.th>
2020-04-19 15:41:25 +0700

    Fix UI bugs
    (More like rework the whole file lmao.)

--- team-based-robot/lib/views/StoreView.js
+++ team-based-robot/lib/views/StoreView.js
@@ -19,8 +19,8 @@ export default class StoreView {
     this.sharingKeywords = this.teambaseInstance.sharingKeywords
     this.localKeywords = []
     this.filePath = '/resources/local-team-based.robot'
-    this.sharingDisplay = []
-    this.localDisplay = []
+    this.keywordNames = []
+    this.sharingNames = []
     this.localNames = []
     this.temp = []
 
@@ -34,66 +34,41 @@ export default class StoreView {
     await etch.update(this)
   }
 
-  onKeywordClicked (index) {
-    this.temp[index].isLocal = !(this.temp[index].isLocal)
-    console.log(this.temp[index].children[1], 'this.temp[index]')
-    // delete this.temp[index]
-
-    // console.log(this.temp.map(el=>el.isLocal))
-
-    // etch.update(this)
-    this.update()
+  onKeywordClicked (index, type) {
+    if (type === 'local') {
+      this.sharingNames.push(this.localNames.splice(index, 1))
+    } else if (type === 'sharing') {
+      this.localNames.push(this.sharingNames.splice(index, 1))
+    }
+    etch.update(this)
   }
 
   mapKeywords () {
-    // console.log(this.localKeywords, 'local keywords')
-    this.sharingDisplay = this.sharingKeywords.map((kwd, index) => {
-      return  <button className="btn" onClick={((event) => {event.stopPropagation();this.onKeywordClicked(index)})}> {kwd.kwd_name} </button>
-    })
-
-    this.temp = [ ...this.sharingDisplay ]
-    this.sharingKeywords.map((kwd, index) => {
-      // const localNames = this.localKeywords.map(kwd => kwd.name)
-      for (i = 0; i < this.localNames.length; i++) {
-        if (this.localNames[i] === kwd.kwd_name) {
-          this.sharingDisplay[index].isLocal = true
-
-        }
-        console.log(kwd.kwd_name, this.sharingDisplay[index].isLocal)
-      }
-    })
-
-    console.log(this.temp, 'this.temp')
-    this.temp.map(el => {
-      if (el.isLocal !== true) {
-        el.isLocal = false
-      }
-    })
-    // this.sharingDisplay = this.temp.filter(el => el.isLocal !== true)
-    // this.localDisplay = this.temp.filter(el => el.isLocal === true)
+    this.localNames = this.localKeywords.map(kwd => kwd.name)
+    console.log(this.localNames, 'this.localNames')
+    this.sharingNames = this.keywordNames.filter(kwd => !this.localNames.includes(kwd))
+    console.log(this.sharingNames, 'this.sharingNames')
   }
 
   render () {
-    // console.log(this.temp)
-    // console.log(this.sharingDisplay)
-    // console.log(this.localDisplay)
-
-    // this.sharingDisplay = this.temp.filter(el => el.isLocal !== true)
-    // this.localDisplay = this.temp.filter(el => el.isLocal === true)
-
-    const sharingItems = <div> {this.sharingDisplay} </div>
-    const localItems = <div> {this.localDisplay} </div>
-    // const sharingItems = <div> kuy </div>
     return (
       <div>
         StoreView
         <div>
           <div>sharing</div>
-            { this.sharingDisplay.map(el => <div>{el}</div>)}
+          <div>{this.sharingNames.map((name, index) => {
+            return <button className="btn" onClick={() => this.onKeywordClicked(index, 'sharing')}> {name} </button>
+          })}
+          </div>
+
         </div>
         <div>
           <div>local</div>
-          { this.localDisplay.map(el => <div>{el}</div>)}
+          <div>
+            {this.localNames.map((name, index) => {
+              return <button className="btn" onClick={() => this.onKeywordClicked(index, 'local')}> {name} </button>
+            })}
+          </div>
         </div>
       </div>
     )
@@ -102,14 +77,10 @@ export default class StoreView {
   show () {
     console.log(this)
     this.sharingKeywords = this.teambaseInstance.sharingKeywords
+    this.keywordNames = this.sharingKeywords.map(kwd => kwd.kwd_name)
     this.localKeywords = parseKeywordSelection(fs.readFileSync(`${getRootDirPath()}/${this.filePath}`).toString())
-    this.localNames = this.localKeywords.map(kwd => kwd.name)
-    this.sharingDisplay = []
-    this.localDisplay = []
-    this.update()
     this.mapKeywords()
-    console.log(this.sharingDisplay)
-    console.log(this.localDisplay)
+    this.update()
     if (!(this.panel.isVisible())) this.panel.show()
     else this.panel.hide()
   }
--- team-based-robot/resources/local-team-based.robot
+++ team-based-robot/resources/local-team-based.robot
@@ -1,25 +1,14 @@
 *** Keywords ***
-Open Browser To Landing Page
-    [Arguments]    ${BROWSER}=chrome
-    Open browser    ${WEB_BASE_URL}    ${BROWSER}
-    Set Selenium Speed    ${SPEED}
+Fill Input Login Form
+	[Arguments]	${username}	${password}
+	Input Text	${login_input_username}	${username}
+	Input Password	${login_input_password}	${password}
+	Click Element	${login_button_submit}
+	Sleep	2
 
-Change Resolution To Desktop
-    Set Window Size    1400    860
+Change Resolution To Mobile
+	Set Window Size	375	667
 
-Change Resolution To Tablet
-    Set Window Size    800    1024
-
-# Change Resolution To Mobile
-#     Set Window Size    375    667
-#
-# Go To Login Page
-#     Go To    ${WEB_BASE_URL}/login
-#     Wait Until Element Is Visible    ${login_div_block}
-#
-# Fill Input Login Form
-#     [Arguments]    ${username}    ${password}
-#     Input Text    ${login_input_username}    ${username}
-#     Input Password    ${login_input_password}    ${password}
-#     Click Element    ${login_button_submit}
-#     Sleep    2
+Go To Login Page
+	Go To	${WEB_BASE_URL}/login
+	Wait Until Element Is Visible	${login_div_block}
